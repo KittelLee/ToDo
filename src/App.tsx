d31b6ecd0dd8c7aea0a3947c8d3e8dbf3b3991fb
Leejinuk <leejin7900@gmail.com>
2024-09-04 15:53:11 +0900

    Feat: TodoAdd Add basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Clock from "./components/Clock";
 import "./App.css";
+import TodoAdd from "./components/TodoAdd";
 
 function App() {
   return (
     <div className="todo-wrap">
       <Clock />
+      <TodoAdd />
     </div>
   );
 }
--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,5 +1,14 @@
+import "../styles/TodoAdd.css";
+
 function TodoAdd() {
-  return <div>TodoAdd</div>;
+  return (
+    <div className="register-wrap">
+      <div className="register-inner">
+        <input type="text" placeholder="할일을 입력하세요" />
+        <button>등록하기</button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoAdd;
