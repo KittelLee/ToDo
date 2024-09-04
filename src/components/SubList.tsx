0a50e5f0c4c098630eefe306e1d537304db07910
Leejinuk <leejin7900@gmail.com>
2024-09-04 17:34:21 +0900

    Refact: common component division

---
+++ src/components/SubList.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import trash from "../assets/icons/trash.svg";
+import modify from "../assets/icons/modify.svg";
+import "../styles/SubList.css";
+
+function SubList() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked((prev) => !prev);
+  };
+
+  return (
+    <div className="sub-list">
+      <input
+        type="checkbox"
+        id="cbx"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <label htmlFor="cbx" className="check">
+        <svg viewBox="0 0 18 18">
+          <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z" />
+          <polyline points="1 9 7 14 15 4" />
+        </svg>
+      </label>
+      {isChecked ? <del>블로그 올리기</del> : <p>블로그 올리기</p>}
+      <img src={modify} alt="수정아이콘" />
+      <img src={trash} alt="삭제아이콘" />
+    </div>
+  );
+}
+
+export default SubList;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,52 +1,15 @@
 import "../styles/TodoList.css";
-import trash from "../assets/icons/trash.svg";
-import modify from "../assets/icons/modify.svg";
+import SubList from "../components/SubList";
 
 function TodoList() {
   return (
     <div className="list-wrap">
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>블로그 올리기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>휴대폰 바꾸기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>과제하기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>씻기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>놀기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>코딩하기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
-      <div className="sub-list">
-        <input type="checkbox" />
-        <p>밥먹기</p>
-        <img src={modify} alt="수정아이콘" />
-        <img src={trash} alt="삭제아이콘" />
-      </div>
+      <SubList />
+      <SubList />
+      <SubList />
+      <SubList />
+      <SubList />
+      <SubList />
     </div>
   );
 }
